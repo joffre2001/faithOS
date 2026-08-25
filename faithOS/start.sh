774947814982f5edc2af6304f236555da601ce9d
Obenson Joffre <obensonjoffre02@gmail.com>
2026-08-25 19:16:44 -0300

    adjusted

--- faithOS/start.sh
+++ faithOS/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-export DB_URL="jdbc:$(echo "$DATABASE_URL" | sed 's#^postgres://#postgresql://#')"
+export DB_URL="jdbc:$(echo "$DATABASE_URL" | sed -E 's#^postgres(ql)?://[^@]*@#postgresql://#')"
 exec java -jar /app/app.jar
