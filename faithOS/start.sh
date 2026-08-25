7d844154f6b76492e1c9b1fd1ce0a835c081561c
Obenson Joffre <obensonjoffre02@gmail.com>
2026-08-25 19:44:25 -0300

    ajust

--- faithOS/start.sh
+++ faithOS/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 export DB_URL="jdbc:$(echo "$DATABASE_URL" | sed -E 's#^postgres(ql)?://[^@]*@#postgresql://#')"
-exec java -jar /app/app.jar
+exec java -XX:TieredStopAtLevel=1 -Xss512k -jar /app/app.jar
